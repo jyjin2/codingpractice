4f979d12a908126a84ffd983edfcbf5b3bc76c4c
Yejin Jang <97579040+jyjin2@users.noreply.github.com>
2025-09-27 11:19:25 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,8 @@
 def solution(array, commands):
     answer = []
     for i, j, k in commands:
+        # i-1부터 j까지 (j 포함 x)
         arr = sorted(array[i-1:j])
+        # k번째 수 구하기
         answer.append(arr[k-1])
     return answer
