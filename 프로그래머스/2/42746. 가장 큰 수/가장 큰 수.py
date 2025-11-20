7474f30adfc3710813d344e5d79416b4f0933ee8
Yejin Jang <97579040+jyjin2@users.noreply.github.com>
2025-11-20 15:48:44 +0900

    [level 2] Title: 가장 큰 수, Time: 61.19 ms, Memory: 26.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,9 +1,9 @@
-def solution(numbers):
+def solution(numbers):  
     # 숫자를 문자열로 변환
     strr = list(map(str, numbers))
     # 문자열을 3번 반복해서 비교
     # 원소는 최대 1000이므로 문자열 길이 최대 4자리
-    # reverse=True 내림차순으로 정렬
+    # reverse =True 내림차순으로 정렬
     strr.sort(key=lambda x: x*3, reverse=True)
     # 모든 원소가 '0'일 경우 '000...'이 아닌 '0' 반환
     return '0' if strr[0] == '0' else ''.join(strr)
