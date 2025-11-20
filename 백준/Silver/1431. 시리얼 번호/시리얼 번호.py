e45f03493961b5362c16d5f0259304a092b6367a
Yejin Jang <97579040+jyjin2@users.noreply.github.com>
2025-11-20 14:33:16 +0900

    [Silver III] Title: 시리얼 번호, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1431. 시리얼 번호/시리얼 번호.py
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.py
@@ -2,31 +2,32 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+serial = [ input().strip() for _ in range(n) ]
 
-def num_sum(arr):
-    ans = 0
-    for i in arr:
-        if i.isdigit():
-            ans += int(i)
-    return ans
-
-serial = [input().strip() for _ in range(n)]
-
+# 버블 정렬
 for i in range(n):
     for j in range(i+1, n):
-        # 1. 길이가 짧은 것이 먼저
+        # 1. 길이가 다르면 짧은 거 먼저
         if len(serial[i]) > len(serial[j]):
             serial[i], serial[j] = serial[j], serial[i]
-            continue
-        # 2. 길이가 같다면 숫자들의 합이 작은 게 먼저
-        if len(serial[i]) == len(serial[j]):
-            if num_sum(serial[i]) > num_sum(serial[j]):
+            
+        # 2. 길이 같다면 숫자의 합이 더 작은 것이 먼저
+        elif len(serial[i]) == len(serial[j]):
+            sumA = 0
+            sumB = 0
+            for x, y in zip(serial[i], serial[j]):
+                if x.isdigit():
+                    sumA += int(x)
+                if y.isdigit():
+                    sumB += int(y)
+            if sumA > sumB:
                 serial[i], serial[j] = serial[j], serial[i]
-                continue
-            # 3. 같으면 사전순으로 비교 (숫자 먼저)
-            if num_sum(serial[i]) == num_sum(serial[j]):
+            elif sumA == sumB:
                 if serial[i] > serial[j]:
                     serial[i], serial[j] = serial[j], serial[i]
-            
+
 for s in serial:
     print(s)
+            
+        
+
